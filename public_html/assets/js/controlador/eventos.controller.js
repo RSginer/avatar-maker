a6cb2cc8f2cf766304a8a4dd0366d1d223d52f4d
Rubén Soler <r.solerginer@gmail.com>
2017-02-05 21:54:50 +0100

    wip: maquina de estados

--- public_html/assets/js/controlador/eventos.controller.js
+++ public_html/assets/js/controlador/eventos.controller.js
@@ -9,22 +9,22 @@ drag = function (ev) {
 function drop(ev) {
     ev.preventDefault();
     var data = JSON.parse(ev.dataTransfer.getData("object"));
-    if (app.estado.avatar[data.tipo].puesta === false) {
-        var img = document.createElement('img');
-        $(img).prop('tipo', data.tipo);
-        img.src = data.src;
-        img.setAttribute('id', data.id);
-        $(img).addClass('avatar');
-        $(ev.target.parentNode).prepend(img);
+    if (app.estado.avatar[data.tipo].puesta === true) {
+         document.getElementById(app.estado.avatar[data.tipo].object.id).setAttribute('id', data.id);
+         document.getElementById(data.id).setAttribute('src', data.src);
     } else {
-        $('#' + app.estado.avatar[data.tipo].object.id).remove();
-        var img = document.createElement('img');
-        $(img).prop('tipo', data.tipo);
-        img.src = data.src;
-        img.setAttribute('id', data.id);
-        $(img).addClass('avatar');
-        $(ev.target.parentNode).prepend(img);
-
+        var imagen = factoryImagen(data);
+        $(ev.target.parentNode).prepend(imagen);
     }
+
     app.estado.avatar[data.tipo] = {object: data, puesta: true};
+}
+
+function factoryImagen(data) {
+    var imagen = document.createElement('img');
+    $(imagen).prop('tipo', data.tipo);
+    imagen.src = data.src;
+    imagen.setAttribute('id', data.id);
+    $(imagen).addClass('avatar');
+    return imagen;
 }
